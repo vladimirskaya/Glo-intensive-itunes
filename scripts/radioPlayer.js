5435a1e7933752f9a766a6dd2d0c0f70e66306ec
Nina Vladimirskaya <naina148@mail.ru>
2020-12-23 19:08:31 +0300

    Adding radioplayer code

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,4 +1,61 @@
 /* вызов плеера радио */
 export const radioPlayerInit = () => {
-    console.log('Radio Init')
+    const radio = document.querySelector('.radio');
+    const radioNavigation = document.querySelector('.radio-navigation');
+    const radioCoverImg = document.querySelector('.radio-cover__img');
+    const radioItem = document.querySelectorAll('.radio-item');
+    const radioHeaderBig = document.querySelector('.radio-header__big');
+    const radioStop = document.querySelector('.radio-stop');
+
+    const audio = new Audio();
+    audio.type = 'audio/aac';
+
+    radioStop.disable = true;
+
+    const changeIconPlay = () => {
+        if (audio.paused) {
+            radio.classList.remove('play');
+            radioStop.classList.add('fa-play');
+            radioStop.classList.remove('fa-stop')
+        } else {
+            radio.classList.add('play');
+            radioStop.classList.remove('fa-play');
+            radioStop.classList.add('fa-stop')
+        }
+    }
+     
+    const selectItem = elem => {
+        radioItem.forEach(item => item.classList.remove('select'));
+        elem.classList.add('select');
+    }
+
+
+    radioNavigation.addEventListener('change', event => {
+        const target = event.target;
+        const parent = target.closest('.radio-item');
+        selectItem(parent);
+
+        const title = parent.querySelector('.radio-name').textContent;
+        radioHeaderBig.textContent = title;
+
+        const urlImg = parent.querySelector('.radio-img').src;
+        radioCoverImg.src = urlImg
+
+        radioStop.disable = false;
+        console.log(parent);
+        audio.src = target.dataset.radioStantion;
+        audio.play();
+        changeIconPlay();
+    });
+
+    radioStop.addEventListener('click', () => {
+        if (audio.paused) {
+            audio.play();
+        } else {
+            audio.pause();
+        }
+        changeIconPlay();
+    })
+
 };
+
