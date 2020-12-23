9962111e6a75a5326698172bf80ba3b612d72253
Nina Vladimirskaya <naina148@mail.ru>
2020-12-23 19:22:12 +0300

    Adding comments to the code

--- scripts/radioPlayer.js
+++ scripts/radioPlayer.js
@@ -1,5 +1,6 @@
 /* вызов плеера радио */
 export const radioPlayerInit = () => {
+    // Получение элементов ДОМ-дерева
     const radio = document.querySelector('.radio');
     const radioNavigation = document.querySelector('.radio-navigation');
     const radioCoverImg = document.querySelector('.radio-cover__img');
@@ -7,33 +8,41 @@ export const radioPlayerInit = () => {
     const radioHeaderBig = document.querySelector('.radio-header__big');
     const radioStop = document.querySelector('.radio-stop');
 
+    //Создание нового объекта Аудио
     const audio = new Audio();
     audio.type = 'audio/aac';
 
+    //делаем неактивной кнопку Плей
     radioStop.disable = true;
 
+    // Изменение состояния иконки и формата анимации
     const changeIconPlay = () => {
         if (audio.paused) {
+            //при остановке вращение пластинки останавливается
             radio.classList.remove('play');
+            // а также меняется иконка кнопки Плей
             radioStop.classList.add('fa-play');
             radioStop.classList.remove('fa-stop')
         } else {
+            //при проигрывании происходит вращение пластинки
             radio.classList.add('play');
+            // а также меняется иконка кнопки Плей
             radioStop.classList.remove('fa-play');
             radioStop.classList.add('fa-stop')
         }
     }
      
+    // Функция обеспечивает выделение только выбранной станции серым кружком
     const selectItem = elem => {
         radioItem.forEach(item => item.classList.remove('select'));
         elem.classList.add('select');
     }
 
-
+    //Функция действует при нажатии на выбранную станцию и воспроизводит ее
     radioNavigation.addEventListener('change', event => {
-        const target = event.target;
-        const parent = target.closest('.radio-item');
-        selectItem(parent);
+        const target = event.target; 
+        const parent = target.closest('.radio-item'); 
+        selectItem(parent); 
 
         const title = parent.querySelector('.radio-name').textContent;
         radioHeaderBig.textContent = title;
@@ -48,6 +57,7 @@ export const radioPlayerInit = () => {
         changeIconPlay();
     });
 
+    //Функция выполняется при нажатии на кнопку воспроизведения/стоп
     radioStop.addEventListener('click', () => {
         if (audio.paused) {
             audio.play();
