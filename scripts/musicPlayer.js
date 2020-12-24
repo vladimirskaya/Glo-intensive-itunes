7da3d403ae8681f970d11eb48bbee5155a9a96ec
Nina Vladimirskaya <naina148@mail.ru>
2020-12-24 22:50:01 +0300

    Adding music player

--- scripts/musicPlayer.js
+++ scripts/musicPlayer.js
@@ -1,4 +1,70 @@
 /* вызов музыкального плеера */
 export const musicPlayerInit = () => {
-    console.log('Music Init')
-};
+    const audio = document.querySelector('.audio');
+    const audioImg = document.querySelector('.audio-img');
+    const audioHeader = document.querySelector('.audio-header');
+    const audioPlayer = document.querySelector('.audio-player');
+    const audioNavigation = document.querySelector('.audio-navigation');
+    const audioButtonPlay = document.querySelector('.audio-button__play');
+    const audioProgress = document.querySelector('.audio-progress');
+    const audioTimePassed = document.querySelector('.audio-time__passed');
+    const audioProgressTiming = document.querySelector('.audio-progress__timing');
+    const audioTimeTotal = document.querySelector('.audio-time__total');
+
+    const playList = ['hello', 'flow', 'speed'];
+
+    let trackIndex = 0;
+
+    const loadTrack = () => {
+        const isPlayed = audioPlayer.paused;
+        const track = playList[trackIndex];
+
+        audioPlayer.src = `./audio/${track}.mp3`;
+        audioHeader.textContent = track.toUpperCase();
+        audioImg.src = `./audio/${track}.jpg`;
+        if (isPlayed) {
+            audioPlayer.pause();
+        } else {
+            audioPlayer.play();
+        }
+    }
+
+    audioNavigation.addEventListener('click', event => {
+        const target = event.target;
+
+        if (target.classList.contains('audio-button__play')) {
+            audio.classList.toggle('play');
+            audioButtonPlay.classList.toggle('fa-play');
+            audioButtonPlay.classList.toggle('fa-pause');
+
+            if (audioPlayer.paused) {
+                audioPlayer.play();
+            } else {
+                audioPlayer.pause();
+            }
+            const track = playList[trackIndex];
+            audioHeader.textContent = track.toUpperCase();
+        }
+
+        if (target.classList.contains('audio-button__prev')) {
+            if (trackIndex !== 0) {
+                trackIndex--;
+            } else {
+                trackIndex = playList.length - 1;
+            }
+            loadTrack();
+        }
+
+        if (target.classList.contains('audio-button__next')) {
+            if (trackIndex === playList.length - 1) {
+                trackIndex = 0;
+            } else {
+                trackIndex++;
+            }
+            loadTrack();
+        }
+
+        
+    });
+
+};  
